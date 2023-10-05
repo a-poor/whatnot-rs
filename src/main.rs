11e7320e6874096974c84a7d774f56226f4f9a01
Austin Poor <45295232+a-poor@users.noreply.github.com>
2023-10-05 16:09:59 -0700

    Creates basic index page with new deps
    - Adds Rust deps maud and axum-extra
    - Adds HTMX and TailwindCSS links
    - Creates some Maud components for head/header

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,66 @@
 use axum::{routing::get, Router};
+use maud::{html, Markup, DOCTYPE};
 
-async fn hello_world() -> &'static str {
-    "Hello, world!"
+
+fn head(title: &str, more: Option<Markup>) -> Markup {
+  html! {
+    head {
+      meta charset="utf-8";
+      meta name="viewport" content="width=device-width, initial-scale=1" {}
+      title { (title) }
+      script src="https://unpkg.com/htmx.org@1.9.6" {}
+      script src="https://cdn.tailwindcss.com" {}
+      (more.unwrap_or_default())
+    }
+  }
+}
+
+fn header(active_tab: Option<&str>) -> Markup {
+  let tabs = [
+    ("Home", "/", active_tab == Some("home")),
+    ("About", "/about", active_tab == Some("about")),
+    ("Login", "/login", active_tab == Some("login")),
+  ];
+  html! {
+    header  {
+      div.flex."max-w-7xl"."mx-auto" {
+        a href="/" {
+          "WhatNow?"
+        }
+        div."flex-grow" {}
+        nav {
+          ul.flex."space-x-4" {
+            @for (label, href, active) in tabs.iter() {
+              li.{({ if *active { "bg-gray-900" } else { "bg-gray-700" } })} {
+                a href=(href) {
+                  (label)
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+async fn index() -> Markup {
+  html! {
+    (DOCTYPE)
+    html lang="en" {
+      (head("WhatNow", None))
+      body {
+        (header(Some("home")))
+        h1 { "Hello, world!" }
+      }
+    }
+  }
 }
 
 #[shuttle_runtime::main]
 async fn axum() -> shuttle_axum::ShuttleAxum {
-    let router = Router::new().route("/", get(hello_world));
-
-    Ok(router.into())
+  let router = Router::new()
+    .route("/", get(index))
+  ;
+  Ok(router.into())
 }
